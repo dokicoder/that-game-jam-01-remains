5b2739272f0b26f52ee090e89b17c3974b3edd83
Kay Illner <kay.illner@gmail.com>
2026-01-02 10:58:05 +0100

    board with animated connectors

--- game/base_board.gd
+++ game/base_board.gd
@@ -1,15 +1,20 @@
 extends Node2D
 
-@export var SocketScene: PackedScene
+@export var Socket: PackedScene
+@export var GreenConnector: PackedScene
+@export var RedConnector: PackedScene
 
 @export_range(1, 20) var width: int = 10
 @export_range(1, 20) var height: int = 10
+
+@export var green_positions: Array[Vector2i] = []
+@export var red_positions: Array[Vector2i] = []
 
 @onready var center_node = $CenterNode; 
 
 func _ready():
 	# TODO: how to get size without instantiating?
-	var reference_socket: Sprite2D = SocketScene.instantiate()
+	var reference_socket: Node2D = Socket.instantiate()
 	
 	var sprite_width = reference_socket.texture.get_width()
 	var sprite_height = reference_socket.texture.get_height()
@@ -21,7 +26,23 @@ func _ready():
 	
 	for x in range(width):
 		for y in range(height):
-			var socket: Sprite2D = SocketScene.instantiate()
+			var socket: Node2D = Socket.instantiate()
 			center_node.add_child(socket)
 			socket.position.x = x * sprite_width
 			socket.position.y = y * sprite_height
+			
+	for connector_position in green_positions:
+		assert(connector_position.x < width)
+		assert(connector_position.y < height)
+		var connector: Node2D = GreenConnector.instantiate()
+		center_node.add_child(connector)
+		connector.position.x = connector_position.x * sprite_width
+		connector.position.y = connector_position.y * sprite_height
+	
+	for connector_position in red_positions:
+		assert(connector_position.x < width)
+		assert(connector_position.y < height)
+		var connector: Node2D = RedConnector.instantiate()
+		center_node.add_child(connector)
+		connector.position.x = connector_position.x * sprite_width
+		connector.position.y = connector_position.y * sprite_height
