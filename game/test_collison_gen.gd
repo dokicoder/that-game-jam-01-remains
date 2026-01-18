312c2ab65e969d05cee1c1e44fbfa14e13fd2de7
Kay Illner <kay.illner@gmail.com>
2026-01-18 13:03:21 +0100

    wip: joining polygons alright

--- game/test_collison_gen.gd
+++ game/test_collison_gen.gd
@@ -23,10 +23,11 @@ func generate_collision_shape():
 		
 		if Geometry2D.is_polygon_clockwise(polygon):
 			print("=> Clockwise")
+			
+			polygon = Transform2D(0, Vector2(-16.0, -16.0)) * polygon
 			var collider = CollisionPolygon2D.new()
 			collider.polygon = polygon
-			collider.position.x = -16
-			collider.position.y = -16
+			
 			add_child(collider)
 			
 			collider.owner = get_tree().edited_scene_root
@@ -36,13 +37,27 @@ func generate_collision_shape():
 func join_with_other_collider():
 	print("join_with_other_collider")
 	var c_this: CollisionPolygon2D = get_child(0)
+	
+	var polygon_this = get_parent().get_relative_transform_to_parent(get_parent().get_parent()) * c_this.polygon
+	var polygon_other = other_collider.get_parent().get_relative_transform_to_parent(get_parent().get_parent()) * other_collider.polygon
+	
+	var polygons = Geometry2D.merge_polygons(polygon_this, polygon_other)
 
-	var polygons = Geometry2D.merge_polygons(c_this.polygon, other_collider.polygon)
+	for c in get_children():
+		c.queue_free()
 
 	for polygon in polygons:
-		
 		if Geometry2D.is_polygon_clockwise(polygon):
 			print("=> Clockwise")
-			c_this.polygon = polygon
+			var collider = CollisionPolygon2D.new()
+			collider.polygon = polygon
+			add_child(collider)
+			
+			collider.owner = get_tree().edited_scene_root
 		else:
 			print(": Counterclockwise")
+			var collider = CollisionPolygon2D.new()
+			collider.polygon = polygon
+			add_child(collider)
+			
+			collider.owner = get_tree().edited_scene_root
