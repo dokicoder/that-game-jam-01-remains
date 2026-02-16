cc74d68bde553620799199c3bc9ac4ac61758189
Kay Illner <kay.illner@gmail.com>
2026-02-16 13:18:19 +0100

    make zooming possible

--- game/debug_camera_control.gd
+++ game/debug_camera_control.gd
@@ -5,6 +5,7 @@ var start_camera_position: Vector2
 var _is_dragging_camera = false
 
 var CAMERA_DRAG_SPEED = 0.2
+var CAMERA_ZOOM_STEP = 0.95
 
 func _input(event):
 	if event is InputEventMouseButton:
@@ -13,6 +14,12 @@ func _input(event):
 			click_position = get_global_mouse_position()
 		if event.button_index == MOUSE_BUTTON_RIGHT and not event.pressed:
 			_is_dragging_camera = false
+		# zoom in
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			self.zoom /= CAMERA_ZOOM_STEP
+		# zoom out
+		if event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			self.zoom *= CAMERA_ZOOM_STEP
 
 func _process(delta: float) -> void:
 	if _is_dragging_camera:
